ee1ecf9b2f7f7bc22709ea4cbf10c641f72dd293
diegobarrera <dieg_goo@hotmail.com>
2016-06-12 19:56:51 -0500

    Remove unused function on CubeController

--- app/controllers/CubeController.php
+++ app/controllers/CubeController.php
@@ -3,23 +3,6 @@
 class CubeController extends BaseController {
 
 	protected $layout = 'layouts.application';
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'CubeController@show');
-	|
-	*/
-
-	public function show(){
-		return View::make('hello');
-	}
 
 	/*
 	|	Route::post('/cube_summation', 'CubeController@calculate');
